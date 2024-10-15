c35e28171ba3d16303e816f727f65a3e38bd1c4b
Paulo Fernandes <paulor.exe@gmail.com>
2024-10-14 23:15:18 -0300

    feat: creating initial styling

--- src/app/components/layout-login-page/layout-login-page.component.ts
+++ src/app/components/layout-login-page/layout-login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-layout-login-page',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './layout-login-page.component.scss'
 })
 export class LayoutLoginPageComponent {
-
+  @Input() title: string = '';
 }
